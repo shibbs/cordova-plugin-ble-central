f3d632b994194ec6190956d2b455b047fadc5e60
shibbs <steve@alpinelaboratories.com>
2017-08-04 10:50:25 -0600

    small change so that I can check it's the right version

--- src/android/BLECentralPlugin.java
+++ src/android/BLECentralPlugin.java
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//FLAH SAH this is the updated plugin as of AUg 4
 package com.megster.cordova.ble.central;
 
 import android.Manifest;
